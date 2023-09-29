4b329ededb99e0940e0c2e107ed9cdc8e5745220
Adesina Abdulrahman <103801534+timmyades3@users.noreply.github.com>
2023-09-29 15:42:30 +0100

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['hng-stage-5-gyzs.vercel.app','hng-stage-5-71phcy1y3-timmyades3.vercel.app','hng-stage-5-ggdtohin8-timmyades3.vercel.app','localhost']
+ALLOWED_HOSTS = ['hng-stage-5-gyzs.vercel.app','hng-stage-5-71phcy1y3-timmyades3.vercel.app','hng-stage-5-ggdtohin8-timmyades3.vercel.app','hng-stage-5.onrender.com','localhost']
 
 
 # Application definition
