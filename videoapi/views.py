2a0f7ef5a30855fc7f9eef6d88a7839a41b19cfc
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-10-02 20:17:26 +0100

    'commit

--- videoapi/views.py
+++ videoapi/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from .tasks import transcribe_video
+# from .tasks import transcribe_video
 from .models import Video
 from .serializers import UploadVideoSerializer
 from tempfile import NamedTemporaryFile, TemporaryFile
